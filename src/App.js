2760bd4bf708105ca002cb3bc267178abea47e3f
ozaki <fs65ugumori@yahoo.co.jp>
2019-12-05 08:22:10 +0900

    絵文字を選んで一覧に出すだけ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import "emoji-mart/css/emoji-mart.css";
-import { Picker } from "emoji-mart";
+import React, { useState } from 'react';
+import 'emoji-mart/css/emoji-mart.css';
+import { Emoji, Picker } from 'emoji-mart';
 
 function App() {
+  const [emojiList, setEmojiList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const onClick = () => setOpen(true);
+  const onSelect = emoji => {
+    console.log({ emoji });
+    setEmojiList([...emojiList, emoji]);
+    setOpen(false);
+  };
   return (
-    <div>
-      <Picker />
-    </div>
+    <>
+      <button onClick={onClick}>Open</button>
+      {open && (
+        <div>
+          <Picker onSelect={onSelect} />
+        </div>
+      )}
+      {emojiList.length ? (
+        <p>
+          {emojiList.map(({ id }, i) => (
+            <Emoji emoji={id} size={24} key={i} />
+          ))}
+        </p>
+      ) : null}
+    </>
   );
 }
 
