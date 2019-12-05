a52bc1dee069680bb729781f71eb7249f7da18fa
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-12-05 12:56:21 +0900

    絵文字の種類出し分け

--- src/EmojiPicker.js
+++ src/EmojiPicker.js
@@ -10,10 +10,19 @@ function EmojiPicker({ onSelect, set }) {
         onSelect={emoji => onSelect({ ...emoji, set })}
         set={set}
         i18n={{
+          search: '検索',
           categories: {
             search: '検索結果',
-            flags: 'Flags',
-            custom: 'Custom',
+            recent: 'よく使う絵文字',
+            people: '顔 & 人',
+            nature: '動物 & 自然',
+            foods: '食べ物 & 飲み物',
+            activity: 'アクティビティ',
+            places: '旅行 & 場所',
+            objects: 'オブジェクト',
+            symbols: '記号',
+            flags: '旗',
+            custom: 'カスタム',
           },
         }}
       />
