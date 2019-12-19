a8693a38d2f45d52752422e20f52156f78b47b57
ozaki <fs65ugumori@yahoo.co.jp>
2019-12-20 00:14:04 +0900

    add sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
-import { Emoji } from 'emoji-mart';
-import EmojiPicker from './EmojiPicker';
+import { Emoji, Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
+
+const emojiTypeList = [
+  'apple',
+  'google',
+  'twitter',
+  'emojione',
+  'messenger',
+  'facebook',
+];
 
 function App() {
-  const [emojiType, setEmojiType] = useState(null);
   const [emojiList, setEmojiList] = useState([]);
+  const [emojiType, setEmojiType] = useState(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const onClickButton = e => {
+    setEmojiType(e.target.name);
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
 
   const onSelect = emoji => {
     console.log({ emoji });
@@ -14,21 +29,51 @@ function App() {
   return (
     <>
       <p>
-        <button onClick={() => setEmojiType('apple')}>Apple</button>
-        <button onClick={() => setEmojiType('google')}>Google</button>
-        <button onClick={() => setEmojiType('twitter')}>Twitter</button>
-        <button onClick={() => setEmojiType('facebook')}>Facebook</button>
-        <button onClick={() => setEmojiType('emojione')}>emojione</button>
-        <button onClick={() => setEmojiType('messenger')}>messenger</button>
+        {emojiTypeList.map(name => (
+          <button onClick={onClickButton} name={name} key={name}>
+            {name}
+          </button>
+        ))}
       </p>
-      {emojiType && <EmojiPicker onSelect={onSelect} set={emojiType} />}
-      {emojiList.length ? (
-        <p>
-          {emojiList.map(({ id, set }, i) => (
-            <Emoji emoji={id} size={24} key={i} set={set} />
-          ))}
-        </p>
-      ) : null}
+      {emojiType && (
+        <Picker
+          onSelect={emoji => onSelect({ ...emoji, emojiType })}
+          set={emojiType}
+          i18n={{
+            search: '検索',
+            categories: {
+              search: '検索結果',
+              recent: 'よく使う絵文字',
+              people: '顔 & 人',
+              nature: '動物 & 自然',
+              foods: '食べ物 & 飲み物',
+              activity: 'アクティビティ',
+              places: '旅行 & 場所',
+              objects: 'オブジェクト',
+              symbols: '記号',
+              flags: '旗',
+              custom: 'カスタム',
+            },
+          }}
+          style={{
+            position: 'absolute',
+            zIndex: '1',
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+          }}
+        />
+      )}
+      {emojiList.length
+        ? emojiList.map(({ id, emojiType }, i) => (
+            <Emoji
+              emoji={id}
+              size={32}
+              set={emojiType}
+              onClick={emoji => onSelect({ ...emoji, emojiType })}
+              key={i}
+            />
+          ))
+        : null}
     </>
   );
 }
